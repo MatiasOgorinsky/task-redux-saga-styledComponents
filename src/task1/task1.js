2b344ec47d026c548a79965f3d54e71e8d163fa9
MatiasOgorinsky <matiogorinsky@gmail.com>
2022-01-24 15:59:09 +0200

    task 1 added

--- src/task1/task1.js
+++ src/task1/task1.js
@@ -13,28 +13,9 @@ const arr = [
   },
 ];
 
-// Write a function to that will get the array as an argument and
-// return an object like the following
+const map = arr.reduce((next, element) => {
+  return { ...next, [element.id]: element };
+}, {});
 
-// const map = {
-//   1: {
-//     id: 1,
-//     name: "Tel Aviv",
-//   },
-//   2: {
-//     id: 2,
-//     name: "London",
-//   },
-//   3: {
-//     id: 3,
-//     name: "Moscow",
-//   },
-// };
-
-const map = [];
-arr.forEach((elem) => {
-  map[elem.id] = elem;
-});
-
-console.log(map, "displaying the new arrray of objects");
-// Make the function as short as possible. Do not modify the original array.
+document.getElementById("originalArray").innerHTML = JSON.stringify(arr, null, 4);
+document.getElementById("array").innerHTML = JSON.stringify(map, null, 4);
